7df3016bc90dbeea35411a916b7d76af09904d1b
Bozo the geek <bozolesv2@gmail.com>
2026-02-18 19:15:56 +0100

    fix(saves): improve "saves" dump mechanism for GB/GBC/GBA
    and move to do save after rom (because rom could be needed for GBA ;-)

--- gbopyrator/gbopyrator.py
+++ gbopyrator/gbopyrator.py
@@ -186,22 +186,6 @@ def main():
         cr.printer.print("")
         cr.printer.rule("[blue_violet]ROM AND SAVE OPERATIONS")
 
-        if (args.dump_save is not None) and (rom_info["RAM_size"] != 0):
-            if rom_info['cartridge_type'].upper().startswith("GBA"):
-                #set cartridge ram size from rom content in this case and not from cartridge info
-                if args.rom_source is not None:
-                    with open(args.rom_source, "rb") as f:
-                        rom_data = f.read()
-                    rom_info['RAM_size'] = detect_gba_ram_size(rom_data)
-                    cr.printer.success(f"RAM size (detected):\t[dark_cyan]{rom_info['RAM_size']}[/dark_cyan]")
-                cr.dump_save(args.dump_save, parse_size_to_bytes(rom_info['RAM_size']))
-                cr.printer.print(f"Dumped save: {rom_info['full_title']}")
-                if(args.debug and args.quiet):
-                    print(f"Dumped save: {rom_info['full_title']}")
-        elif(args.dump_save is not None):
-                cr.printer.print(f"No save to dump !")
-                if(args.debug and args.quiet):
-                    print("No save to dump !")
         if args.dump_rom is not None:
             if rom_info['cartridge_type'].upper().startswith("GBA"):
                 #set cartridge rom size from rom info in this case and not from cartridge info
@@ -226,6 +210,27 @@ def main():
             cr.printer.print(f"Dumped game: {rom_info['full_title']}")
             if(args.debug and args.quiet):
                 print(f"Dumped game: {rom_info['full_title']}")
+
+        if (args.dump_save is not None) and (rom_info["RAM_size"] != 0):
+            if rom_info['cartridge_type'].upper().startswith("GBA"):
+                #set cartridge ram size from rom content in this case and not from cartridge info if rom available
+                if args.dump_rom is not None:
+                    with open(args.dump_rom, "rb") as f:
+                        rom_data = f.read()
+                    rom_info['RAM_size'] = detect_gba_ram_size(rom_data)
+                    if(args.debug and args.quiet):
+                        print(f"RAM size (detected): {rom_info['RAM_size']}")
+                    cr.printer.success(f"RAM size (detected):\t[dark_cyan]{rom_info['RAM_size']}[/dark_cyan]")
+                cr.dump_save(args.dump_save, parse_size_to_bytes(rom_info['RAM_size']))
+            else:
+                cr.dump_save(args.dump_save)
+            cr.printer.print(f"Dumped save: {args.dump_save}")
+            if(args.debug and args.quiet):
+                print(f"Dumped save: {args.dump_save}")
+        elif(args.dump_save is not None):
+                cr.printer.print(f"No save to dump !")
+                if(args.debug and args.quiet):
+                    print("No save to dump !")
 
         if args.write_save is not None:
             cr.write_save_from_file(args.write_save)
